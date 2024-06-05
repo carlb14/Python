a11d1e4d2b7ad4f5fdd547cc889f4d8c5c433bca
carlb14 <166364118+carlb14@users.noreply.github.com>
2024-06-06 01:19:50 +0800

    Update excel.py

--- excel.py
+++ excel.py
@@ -6,26 +6,20 @@ from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.history import InMemoryHistory
 import datetime
 import os.path
-print('Welcome to Python Automated Tool')
+
+print('\nWelcome to Python Automated Tool\n')
 
 # Function to get the worksheet and workbook from the user
 def Requirements():
     while True:
-        Path = input(
-            '\nEnter the Location or PATH of the file (e.g: /home/carl/workloads.xlsx): ')
-        Choice_sheet = input(
-            '\nEnter the name of the sheet you want to access (e.g: sheet1): ')
+        Path = input('\nEnter the Location or PATH of the file (e.g: /home/carl/workloads.xlsx): ')
         if not os.path.isfile(Path):
             print('\n\033[91mThe file is not found.\033[00m')
         else:
             wb = load_workbook(Path)
-            if Choice_sheet not in wb.sheetnames:
-                print('\n\033[91mThe specified sheet is not found in the workbook.\033[00m')
-                continue
-            else:
-                ws = wb[Choice_sheet]
-                break
-    return ws, wb, Path
+            break
+
+    return wb, Path
 
 # Function to provide autocompletion for user input
 def autocomplete_input(prompt_text, options):
@@ -53,7 +47,7 @@ def autocomplete_input(prompt_text, options):
 def CompanyNameD():
     options = [
         "AFTERSIX DESSET&COFFEE INC", "AMICI FOODSERVICE VENTURES INC", "ANTHEM SHOPPES INC", "ASHLAR SECURITY SERVICES", "BIR", "BISTRO AMERICANO CORP", "BREADTALK PHILIPPINES INC", "BU PARTNERS INC", "CAFE VIA MARE", "CAFFEFRANCE CORP.", "CARA MIA SALES AND SERVICES INC", "CDC RESOURCES, INC", "CFAL OASIS DEVELOPMENT CORPORATION", "CK OF ASIA INC", "COFFEE MASTERS INC", "DECATHLON PHILIPPINES INC", "ENCHANTED KONGDOM INC", "ESPRESSO CONCEPTS INC", "EST. NAMNAM, INC", "EXA FUEL MARKETING OPC", "FOOGY MOUNTAIN COOKHOUSE", "GENMAICHA TEA CORPORATION", "GINGERSNAPS MARKETING IL CONIGLIO BIANCO CORPORATION", "GOLDEN DONUTS INC", "GOLDEN DONUTS, INC", "GOLDEN TEA LEAF INC", "GT & T GASOLINE STATION", "GTI", "HAPPY LEMON GROUP PHILIPPINES INC", "HEALTHY OPTION CORPORATION", "HOLDINGS INC.", "ILAOLLAO", "INTERBIKE MARKETING CORPORATION", "INTERNATIONAL TOYWORLD, INC", "IPPUDO PHILIPPINES, INC", "IT'S TIME FOR TIMS COFFEE INC.", "JBPX FOODS INC.", "JOLLIBEE BAGUIO BONIFACIO NIDEL COUNTRY LINE FOODS CORPORATION", "JOLLIBEE FOODS CORP.", "JOLLIBEE FOODS CORPORATION", "JOLLY888 FOOD CORP.", "JWB GASULINA CORP.", "KAREILA MGNT CORP", "LE PATISSIER INC",
-        "LOLOMBOY WATERWORKS ASSOCIATION, INC", "LUGGAGE TRADING SERVICES INC", "MAJOR SHOPPING MANAGEMENT CORPORATION", "MARIE GALANTE FOOD SERVICES INC.", "MARY GRACE FOODS INC", "MAX'S GROUP INC", "MCDELIGHT SOUTH INC.", "MERCURY DRUG", "MERALCO", "MULTISPORTS INC", "MUNICIPALITY OF BOCAUE", "NATIONAL BOOK STORE, INC", "NOODLERAMA GROUP INC", "NOODLERAMA GROUP INC.", "OMAKASE INC.", "ORTIGAS HOME DEPOT", "OUTLOOK RESIDENCES INC", "PEPER BROERS, INC", "PRETIOLAS PHILIPPINES, INC", "REBEL ALLIANCE FOOD SUPPLY, INC", "REVSTAR TRADING", "RICHPRIME SALES INCORPORATED", "ROTI BOY, INC", "RUSTAN COFFEE CORP.", "RUSTAN COFFEE CORPORATION", "SAN ROQUE SHELL SERVICE STATION", "SEATTLE'S BEST COFFEE MASTER INC", "SPECIALTY FOOD RETAILERS INC", "SPECIALTY LIFESTYLE CONCEPTS, INC", "SUNDAYS EST CAFE AND RESTAURANT INC", "SUNDAYS EST CAFE AND RESTAURANT INC.", "SWITCHSTART INC", "TABLE BLACK KITCHEN CORP.", "TEAMBAKEMARKETING CORP", "THE FRENCH BAKER INC", "THE TWELVE 28TH INC", "TOBISTO FOODSO PHI INC", "TOBISTRO FOODS, INC", "UNIGLIPPLOBE TRAVELWARE CO., INC", "UNIMART", "URBAN TASTE INC", "VIVA INTERNATIONAL FOOD & RESTAURANT, INC", "WILD FLOUR BAKERY+CAFE CORP", "WILDFLOUR BAKERY+CAFE CORP"]
+        "LOLOMBOY WATERWORKS ASSOCIATION, INC", "LUGGAGE TRADING SERVICES INC", "MAJOR SHOPPING MANAGEMENT CORPORATION", "MARIE GALANTE FOOD SERVICES INC.", "MARY GRACE FOODS INC", "MAX'S GROUP INC", "MCDELIGHT SOUTH INC.", "MERCURY DRUG", "MERALCO", "MULTISPORTS INC", "MUNICIPALITY OF BOCAUE", "NATIONAL BOOK STORE, INC", "NOODLERAMA GROUP INC", "NOODLERAMA GROUP INC.", "OMAKASE INC.", "ORTIGAS HOME DEPOT", "OUTLOOK RESIDENCES INC", "PEPER BROERS, INC", "PRETIOLAS PHILIPPINES, INC", "REBEL ALLIANCE FOOD SUPPLY, INC", "REVSTAR TRADING", "RICHPRIME SALES INCORPORATED", "ROTI BOY, INC", "RUSTAN COFFEE CORP.",         "RUSTAN COFFEE CORPORATION", "SAN ROQUE SHELL SERVICE STATION", "SEATTLE'S BEST COFFEE MASTER INC", "SPECIALTY FOOD RETAILERS INC", "SPECIALTY LIFESTYLE CONCEPTS, INC", "SUNDAYS EST CAFE AND RESTAURANT INC", "SUNDAYS EST CAFE AND RESTAURANT INC.", "SWITCHSTART INC", "TABLE BLACK KITCHEN CORP.", "TEAMBAKEMARKETING CORP", "THE FRENCH BAKER INC", "THE TWELVE 28TH INC", "TOBISTO FOODSO PHI INC", "TOBISTRO FOODS, INC", "UNIGLIPPLOBE TRAVELWARE CO., INC", "UNIMART", "URBAN TASTE INC", "VIVA INTERNATIONAL FOOD & RESTAURANT, INC", "WILD FLOUR BAKERY+CAFE CORP", "WILDFLOUR BAKERY+CAFE CORP"]
 
     return autocomplete_input("Company name:", options)
 
@@ -107,7 +101,29 @@ def DateTime(input_str):
     month_abbr = datetime.date(year, month_number, day).strftime("%b")
 
     datetime_number = f"{day}-{month_abbr}-{year}"
-    return datetime_number, input_str
+    return datetime_number, input_str, month_number
+
+def ChoiceSheet(month_number, wb):
+    month_names = {
+        '01': 'January',
+        '02': 'February',
+        '03': 'March',
+        '04': 'April',
+        '05': 'May',
+        '06': 'June',
+        '07': 'July',
+        '08': 'August',
+        '09': 'September',
+        '10': 'October',
+        '11': 'November',
+        '12': 'December'
+    }
+    sheet_name = month_names.get(month_number)
+    if sheet_name:
+        ws = wb[sheet_name]
+        return ws
+    else:
+        print('Not Found')
 
 # Function to get the TIN (Taxpayer Identification Number)
 def TIN_D():
@@ -118,10 +134,19 @@ def TIN_D():
         else:
             TIN = tin_number
             return TIN
-
+def Again():
+    while True:   
+        results = input('\nDo you want to create more (y/n)? ')
+        if results.lower() == 'y':
+            Input_details(input("\nEnter the date (052424 = May,24,2024): "))
+            break
+        else:
+            print('\nGood Bye!')
+            exit() 
+            
 # Function to input details and calculate Cost of Products & Services
 def Input_details(input_str):
-    datetime_number = DateTime(input_str)
+    datetime_number, input_str, month_number = DateTime(input_str)
     company_name = CompanyNameD()  
     address_name = AddressD()
     TIN = TIN_D()
@@ -129,80 +154,64 @@ def Input_details(input_str):
     TotalAmountPaid = float(input('\nEnter the Total Amount Paid: '))
     InputVat = float(input('\nEnter the Input Vat: '))
     CPS = TotalAmountPaid - InputVat
-
-    return datetime_number, company_name, address_name, TIN, description_name, TotalAmountPaid, InputVat, CPS, input_str
-
-# Function to add details to Excel worksheet
-def excel(ws, wb, datetime_number, company_name, address_name, TIN, description_name, TotalAmountPaid, InputVat, CPS, Path):
-
-    font = Font(name="Arial", size="10",)
-    alignment = Alignment(horizontal='center', vertical='center')
-
-    ws.append([datetime_number, company_name, address_name, TIN,
-               description_name, TotalAmountPaid, InputVat, CPS])
-
-    for row in ws.iter_rows(min_row=ws.max_row, max_row=ws.max_row):
-        for cell in row:
-            cell.font = font
-            cell.alignment = alignment
-    wb.save(Path)
-
-# Function to confirm details with the user and encode them into Excel
-def Confirm_details(datetime_number, company_name, address_name, TIN, description_name, TotalAmountPaid, InputVat, CPS, ws, wb, Path, input_str):
-    print('\nConfirm Details\n')
-
+    ws = ChoiceSheet(str(month_number).zfill(2), wb)
+    
+    
+    #COnfirm Details
     print(f'Date: {datetime_number}')
-    print(f'Company_name: {company_name}')
+    print(f'Company: {company_name}')
     print(f'Address: {address_name}')
     print(f'TIN: {TIN}')
     print(f'Description: {description_name}')
-    print(f'TotalAmountPaid: {TotalAmountPaid}')
-    print(f'InputVat: {InputVat}')
+    print(f'Total Amount Paid: {TotalAmountPaid}')
+    print(f'Input Vat: {InputVat}')
     print(f'Cost of Product & Services: {CPS}')
-
-    response = input(
-        '\nPlease enter either y or n. If details are correct enter y, while for n if not: ')
-
-    while response.lower() not in ['y', 'n']:
-        print("\033[91mInvalid input. Enter either y or n.\033[0m")
-        response = input("Check details. Do you want to proceed? (y/n): ")
-
-    if response.lower() == 'y':
-        excel(ws, wb, datetime_number, company_name, address_name, TIN,
-              description_name, TotalAmountPaid, InputVat, CPS, Path)
-
-        print('\nEncoded successfully')
-    else:
-        print('\nRetyping...')
-        Input_details(input_str)
-
-# Function to ask if user wants to input more data
-def AnotherOne(input_str):
-    response = input(
-        '\nDo you want to create more? Enter y for YES, n for NO:  ')
-
-    while response.lower() not in ['y', 'n']:
-        print("\033[91mInvalid input. Enter either y or n.\033[0m")
-        response = input(" Do you want to proceed? (y/n): ")
-
-    if response.lower() == 'y':
-        Input_details(input_str)
-
-    else:
-        print('Good Bye! ')
-        exit()
-
-# Function to execute the encoding process
-def Method():
-    input_str = input("\nEnter the date in MMDDYY format (e.g. 051523): ")
-    datetime_number, company_name, address_name, TIN, description_name, TotalAmountPaid, InputVat, CPS, input_str = Input_details(input_str)
     
-    Confirm_details(datetime_number[0], company_name, address_name, TIN, description_name, TotalAmountPaid, InputVat, CPS, ws, wb, Path, input_str)
-    return input_str
+    
+    
+    while True:
+        y_or_n = input('\nDoes the details are correct? Enter (y/n): ')
+        if y_or_n.lower() == 'y':
+            # Get the next available row
+            next_row = ws.max_row + 1
+
+            # Set values for the new row
+            font = Font(name="Arial", size=10)
+            alignment = Alignment(horizontal='center', vertical='center')
+
+            cells = [
+                (1, datetime_number),
+                (2, company_name),
+                (3, address_name),
+                (4, TIN),
+                (5, description_name),
+                (6, TotalAmountPaid),
+                (7, InputVat),
+                (8, CPS),
+            ]
+
+            for column, value in cells:
+                cell = ws.cell(row=next_row, column=column)
+                cell.value = value
+                cell.font = font
+                cell.alignment = alignment
+            # Save the workbook
+            wb.save(Path)
+            
+            print("\n\033[92mData has been successfully saved.\033[00m")
+            break
+        elif y_or_n.lower() == 'n':
+            Input_details(input("Enter the date (052424 = May,24,2024): "))
+            Again()
+            break
+            
+        else:
+            
+            print("\nInvalid key. Plase enter (y/n)\n")
+
+    
 
 if __name__ == "__main__":
-
-    ws, wb, Path = Requirements()
-    input_str = Method()
-
-    AnotherOne(input_str)
+    wb, Path = Requirements()
+    Input_details(input("Enter the date (052424 = May,24,2024): "))
+    Again()
